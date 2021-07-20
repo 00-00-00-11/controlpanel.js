d1c5ee6965ebd919fb124f58ed485db66e9a80c1
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2021-07-20 09:46:15 +0100

    voucher methods

--- src/client/methods/voucher.js
+++ src/client/methods/voucher.js
@@ -1,28 +1,13 @@
 const ClientRequest = require('../../ClientRequest');
 const axios = require('axios');
 
-class Users extends ClientRequest {
+class Vouchers extends ClientRequest {
 
-    getUsers() {
+    getVouchers() {
         return new Promise((resolve, reject) => {
             axios({
                 method: 'GET',
-                url: `${this.host}/api/users`,
-                maxRedirects: 5,
-                headers: this.headers()
-            }).then(response => {
-                resolve(response.data)
-            }).catch(error => {
-                reject(this.HttpError(error));
-            });
-        });
-    }
-
-    getUserDetails(userid) {
-        return new Promise((resolve, reject) => {
-            axios({
-                method: 'GET',
-                url: `${this.host}/api/users/${userid}`,
+                url: `${this.host}/api/vouchers`,
                 maxRedirects: 5,
                 headers: this.headers()
             }).then(response => {
@@ -33,18 +18,33 @@ class Users extends ClientRequest {
         });
     }
 
-    updateUserDetails(userid, name, email, credits, server_limit, role) {
+    getVouchersDetails(voucherid) {
         return new Promise((resolve, reject) => {
             axios({
-                method: 'PATCH',
-                url: `${this.host}/api/users/${userid}`,
+                method: 'GET',
+                url: `${this.host}/api/vouchers/${voucherid}`,
+                maxRedirects: 5,
+                headers: this.headers()
+            }).then(response => {
+                resolve(response.data);
+            }).catch(error => {
+                reject(this.HttpError(error));
+            });
+        });
+    }
+
+    createVoucher({ memo: memo, code: code, uses: uses, credits: credits, expires: expires}) {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: 'POST',
+                url: `${this.host}/api/vouchers`,
                 maxRedirects: 5,
                 data: {
-                    name: name,
-                    email: email,
+                    memo: memo,
+                    code: code,
+                    uses: uses,
                     credits: credits,
-                    server_limit: server_limit,
-                    role: role
+                    expires_at: expires
                 },
                 headers: this.headers()
             }).then(response => {
@@ -55,11 +55,33 @@ class Users extends ClientRequest {
         });
     }
 
-    deleteUser(userid) {
+    updateUserDetails(voucherid, { memo: memo, code: code, uses: uses, credits: credits, expires: expires}) {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: 'PATCH',
+                url: `${this.host}/api/vouchers/${voucherid}`,
+                maxRedirects: 5,
+                data: {
+                    memo: memo,
+                    code: code,
+                    uses: uses,
+                    credits: credits,
+                    expires_at: expires
+                },
+                headers: this.headers()
+            }).then(response => {
+                resolve(response.data);
+            }).catch(error => {
+                reject(this.HttpError(error));
+            });
+        });
+    }
+
+    deleteVoucher(voucherid) {
         return new Promise((resolve, reject) => {
             axios({
                 method: 'DELETE',
-                url: `${this.host}/api/users/${userid}`,
+                url: `${this.host}/api/vouchers/${voucherid}`,
                 maxRedirects: 5,
                 headers: this.headers()
             }).then(response => {
@@ -71,4 +93,4 @@ class Users extends ClientRequest {
     }
 }
 
-module.exports = Users;
+module.exports = Vouchers;
